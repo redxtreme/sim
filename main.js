631708596e0693f9a53d6b7ccf8f49362533927f
redxtreme <zahopper@mtu.edu>
2015-11-30 12:49:14 -0600

    Clock inside of object
    Self contained clock in each tower

--- main.js
+++ main.js
@@ -1,26 +1,49 @@
 console.log("working");
 var maxRuns = 50;
 var runs = 0;
-var iID = setInterval(clock, 5000);
 var nodes = [];
-
+var iID = setInterval(clock, 1000);
 init();
 
 function init() {
-    var tower1 = new Tower("Lazer");
+    var tower1 = new Tower(50, 25, 0, 100, 50, "Lazer");
     nodes.push(tower1);
-    printProps(tower1);
+    tower1.checkPower();
 }
 
-function Tower(name) {
-    this.happy = 100;
-    this.sad = 0;
-    this.age = 0;
-    this.sick = 0;
-    this.hungry = 0;
-    this.thirsty = 0;
-    this.bathroom = 0;
+function Tower(cost, damage, power, maxPower, upgradeCost, name) {
+    this.cost = cost;
+    this.damage = damage;
+    this.power = power;
+    this.maxPower = maxPower;
+    this.upgradeCost = upgradeCost;
     this.name = name;
+    this.clockID = -1;
+    
+    // Set up the clock
+    this.clock = function () {
+        
+        this.clockID = setInterval(this.incPower.bind(this), 1000);
+    };
+    
+    // Increase power
+    this.incPower = function () {
+                
+        if (this.power < this.maxPower) {
+            this.power += 1;
+        }
+        else {
+            clearInterval(this.clockID);
+        }
+    };
+    
+    // Turn clock on if power lower
+    this.checkPower = function () {
+        
+        if (this.power < this.maxPower) {
+            this.clock();
+        }
+    };
 }
 
 function printProps(tower) {
@@ -31,22 +54,10 @@ function printProps(tower) {
     document.write("]");
 }
 
-function updateStats(node) {
-    node.sick += 1;
-}
-
 function clock() {
-
-    if (runs >= maxRuns) {
-        clearInterval(iID);
-        return 0;
-    }
-    runs += 1;
-    
     document.body.innerHTML = ''; // clear the old data
     
     for (var i = 0; i < nodes.length; i++) {
         printProps(nodes[i]);
-        updateStats(nodes[i]);
     }
 }
